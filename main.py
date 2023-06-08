e784e7727a42b36472c8d959ec1895d4fd06ca1d
Fearkin <fearjin1@gmail.com>
2023-06-08 07:55:42 +0000

    Added handler for records

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import logging
+from os import getenv
+
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher.filters.state import StatesGroup, State
-from aiogram.types import Message
-
-from aiogram_dialog import Window, Dialog, DialogRegistry, DialogManager, StartMode
+from aiogram.dispatcher import filters
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import Message, ContentType
+from aiogram_dialog import Dialog, DialogManager, DialogRegistry, StartMode, Window
 from aiogram_dialog.widgets.kbd import Button
 from aiogram_dialog.widgets.text import Const
 
-from os import getenv
-
 storage = MemoryStorage()
 bot = Bot(token=getenv("BOT_TOKEN"))
+logging.basicConfig(level=logging.INFO)
 dp = Dispatcher(bot, storage=storage)
 registry = DialogRegistry(dp)
 
@@ -29,8 +31,20 @@ registry.register(dialog)
 
 
 @dp.message_handler(commands=["start"])
-async def start(m: Message, dialog_manager: DialogManager):
+async def start(message: Message, dialog_manager: DialogManager):
     await dialog_manager.start(MySG.main, mode=StartMode.RESET_STACK)
+
+
+@dp.message_handler(content_types=ContentType.TEXT)
+async def echo_name(message: Message):
+    message.answer(message.chat.id)
+
+
+@dp.message_handler(filters.RegexpCommandsFilter(
+        regexp_commands=[r"(\d+) (\w+)( \w+)?"]
+))
+async def add_record(message: Message):
+    message.answer("test record")
 
 
 if __name__ == '__main__':
